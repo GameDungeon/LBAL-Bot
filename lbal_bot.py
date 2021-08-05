517a222c84db1785b369fc174cb13c4b14d461b4
GameDungeon <gamedungeon@protonmail.com>
2021-08-05 17:09:21 -0500

    First

---
+++ lbal_bot.py
@@ -0,0 +1,259 @@
+import os
+import time
+from pydirectinput import click, press
+import json
+import numpy as np
+import dictdiffer
+import random
+from copy import copy
+
+from tiles import IDS
+
+save_path = os.getenv('APPDATA') +"\\Godot\\app_userdata\\Luck be a Landlord\\LBAL.save"
+
+nodes = 1000
+
+def startup():
+    os.startfile("steam://rungameid/1404850")
+    time.sleep(2)
+
+
+def new_game():
+    click(961, 540)
+    click(380, 730)
+    click(950, 850)
+    time.sleep(0.5)
+    accept()
+
+
+def add(index):
+    press(str(index))
+
+
+def accept():
+    press("enter")
+
+
+def spin():
+    press("space")
+
+
+def deny():
+    press("backspace")
+
+
+def skip():
+    press("s")
+
+def predict_board(symbols, nodes, layers):
+    returned = []
+    for _ in range(nodes):
+        out = predict_thread(symbols, layers)
+        returned.append(sum(out) / len(out))
+
+    return sum(returned) / len(returned)
+
+def predict_thread(symbols, layers, layer=1):
+    coin = 0
+
+    symbols = copy(symbols)
+    tiles = copy(symbols)
+    tiles.extend([IDS["symbol"]["empty"]] * max(0, 20 - len(tiles)))
+    tiles = np.reshape(random.sample(tiles, 20), (4, 5))
+
+    tiles = np.vectorize(lambda m : copy(m))(tiles)
+
+    removal = set()
+    add = []
+    for y, row in enumerate(tiles):
+        for x, tile in enumerate(row):
+            neighbours = [tiles[yn][xn] 
+                            for xoff in range(-1, 2)
+                            for yoff in range(-1, 2)
+                            if (not (xoff == 0 and yoff == 0)
+                                and 0 <= (xn := x+xoff) < 5
+                                and 0 <= (yn := y+yoff) < 4)]
+
+            for z in neighbours:
+                if z.name in tile.interactions or "any" in tile.interactions:
+                    interaction = tile.interactions[z.name if z.name in tile.interactions else "any"]
+                    if type(interaction) == list:
+                        tile.coin_value += interaction[0][0]
+                        tile.coin_value += interaction[0][1] * z.coin_value if interaction[0][1] != 0 else 0
+                        if interaction[0][2]: removal.add(tile)
+                        interaction = interaction[1]
+
+                    z.coin_value += interaction[0]
+                    z.multiplier *= interaction[1]
+                    if interaction[2]: removal.add(z)
+            
+            for x in tile.chance:
+                if x[0] >= random.random():
+                    tile.coin_value += x[1][0]
+                    if x[1][1] != None: add.extend(x[1][1])
+                    if x[1][2] != None: removal.add(tile)
+            
+            if tile.other != None:
+                tile_names = [m.name for m in tiles.flatten()]
+                if tile.name in tile_names:
+                    count = tile_names.count(tile.name) - 1
+                    for _ in range(count):
+                        tile.coin_value += tile.other
+            
+            if tile.dice != None:
+                tile.coin_value += random.randint(1, tile.dice)
+
+            if tile.self_destruct:
+                removal.add(tile)
+
+
+    coin = np.sum(np.vectorize(lambda m : m.coin_value * m.multiplier)(tiles))
+
+    if removal:
+        symbols = list(symbols)
+        for x in removal:
+            symbols.remove(IDS["symbol"][x.name])
+            add.extend(x.on_destroy_add)
+            coin += x.destruction_coin_bonus * x.multiplier
+
+    symbols.extend([IDS["symbol"][y] for y in add])
+   
+    if layer != layers:
+        return [coin] + predict_thread(symbols, layers, layer+1)
+    return [coin]
+
+def parse_save(last_save, last_info):
+    try:
+        with open(save_path, "r") as save:
+            lines = save.readlines()
+    except IOError:
+        lines = []
+        last_save = {}
+
+    lines = list(map(json.loads, lines))
+    info = dict()
+
+    if last_save != {}:
+        for i, line in enumerate(lines):
+            changes = list(dictdiffer.diff(last_save[i], line))
+            if changes:
+                pass
+                # pprint(changes)
+        # TODO: Inventory Sorts Items value/rarity/alphabet
+
+        symbols = []
+        out = []
+        for i in range(3, 8):
+            symbols.append(lines[i]["icon_types"])
+
+        for l in symbols:
+            out += l
+
+        info["symbols"] = [IDS["symbol"][x] for x in list(filter(lambda a: a != "empty", out))]
+        
+        info["choices"] = lines[9]["saved_card_types"]
+        info["email"]   = lines[9]["emails"]
+        info["remove"]  = lines[9]["removal_tokens"]
+        info["reroll"]  = lines[9]["reroll_tokens"]
+
+        info["coins"]   = lines[2]["coins"]
+
+        rent = lines[9]["rent_values"]
+
+        info["rentCost"]  = rent[0]
+        info["turnsLeft"] = rent[1]
+
+        # if info != last_info:
+        #    pprint(info)
+    else:
+        info = last_info
+
+    return lines, info
+
+
+def bot():
+    save = {}
+    info = {}
+    first_spin = True
+    lass = ""
+
+    while True:
+        x, info = parse_save(save, info)
+        if not x:
+            continue
+        save = x
+        if not info:
+            continue
+
+        time.sleep(0.1)
+
+        if info["email"]:
+            first_spin = True
+            email = info["email"][0]
+            if email["replies"] == ["<icon_confirm>"]:
+                accept()
+
+            elif email["replies"] == ["skip"]:
+                skip()
+
+            elif email["type"] == "rent_due":
+                accept()
+
+            elif email["type"] == "add_tile":
+                tile_num = 1
+                adverage = predict_board(info["symbols"], nodes, info["turnsLeft"])
+                choices = [predict_board(info["symbols"] + [IDS["symbol"][symbol]], nodes, info["turnsLeft"]) for symbol in info["choices"]]
+                max_value = max(choices)
+
+                if max_value >= adverage:
+                    if choices.count(max(choices)) > 1:
+                        indexes = [i for i, element in enumerate(choices) if element == max(choices)]      
+                        max_interactions = -1
+
+                        for x in indexes:
+                            y = len(info["symbols"][x].interactions)
+                            if y > max_interactions:
+                                max_interactions = y
+                                tile_num = x          
+                    else:
+                        tile_num = choices.index(max(choices))
+
+                    add(tile_num + 1)
+
+                    b = max_value, info["choices"][tile_num]
+                else:
+                    skip()
+                    b = "Too Low: " + str(adverage)
+
+                if lass != b:
+                    print(b)
+                    lass = b
+
+            elif email["type"] == "add_item":
+                print(info["choices"][0])
+                add(1)
+
+            elif email["type"] == "comfy_pillow_prompt" or email["type"] == "33":
+                if info["coins"] > info["rentCost"]:
+                    accept()
+                else:
+                    deny()
+
+            elif email["type"] == "oil_can_prompt":
+                deny()
+            elif email["type"] == "swap_prompt_1":
+                deny()
+            elif email["type"] == "game_over":
+                quit()
+            else:
+               print(email)
+
+        else:
+            if first_spin:
+                spin()
+                first_spin = False
+
+
+startup()
+new_game()
+bot()
